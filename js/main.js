b8e0263e1167f4b15e7c8933cbbddb1f65d28508
Hollenbeck <k_hollenbeck@ats-hr420-dm24.fc.ca>
2019-09-09 19:48:02 -0400

    adding js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import Person from "./modules/DataObject.js";
+
+// IIFE - Immediately Invoced Function Expression
+(() => {
+    console.log('fired')
+
+    console.log(Person);
+
+    debugger;
+})();
